0bb60ef31b5efc5e838da684fc22238691bbcb04
Ameek Singh <ameek@Ameeks-MacBook-Air.local>
2023-09-11 13:21:53 -0400

    Assignment 1 - Activity 4: Created utils_tests file with tests for formatter and reversed.

---
+++ utils_tests.py
@@ -0,0 +1,23 @@
+import utilities
+def utils_tests():
+    # testing reversed:
+    # with string
+    assert(utilities.utils.reversed("random-string") == "Error!")
+    # with float
+    assert(utilities.utils.reversed(3.14) == "Error!")
+    # with positive int
+    assert(utilities.utils.reversed(155) == 551)
+    # with negative int
+    assert(utilities.utils.reversed(-155) == -551)
+    
+    # testing formatter:
+    # with string
+    assert(utilities.utils.formatter("random-string") == "Error!")
+    # with float
+    assert(utilities.utils.formatter(3.14) == "Error!")
+    # with positive int
+    assert(utilities.utils.formatter(128) == ('0b10000000', '0o200'))
+    assert(utilities.utils.formatter(135) == ('0b10000111', '0o207'))
+    # with negative int
+    assert(utilities.utils.formatter(-128) == -128)
+
